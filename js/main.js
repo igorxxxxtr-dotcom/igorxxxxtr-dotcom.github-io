370a49b54892e71a2b4f9a6baa57fe068fb34e39
igorxxxxtr-dotcom <igorxxxxtr@gmail.com>
2026-04-05 00:52:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#app',
+
+    data: {
+        products: [
+            {
+                id: 1,
+                title: "Green Onion Classic",
+                short_text: "Свежий зелёный лук",
+                image: "images/cebyl2.jpg"
+            },
+            {
+                id: 2,
+                title: "Green Onion Fresh",
+                short_text: "Сочный и ароматный",
+                image: "images/cebyl3.jpg"
+            },
+            {
+                id: 3,
+                title: "Green Onion Premium",
+                short_text: "Премиум сорт",
+                image: "images/cebyl4.jpg"
+            }
+        ]
+    }
+});
